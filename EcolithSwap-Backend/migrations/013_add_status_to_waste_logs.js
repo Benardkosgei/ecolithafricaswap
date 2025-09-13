f69a8bb83f2e91a8585b12017559f5595c12f357
benardkosgei96 <benardkosgei96@gmail.com>
2025-09-13 20:45:04 +0000

    Status isse

--- EcolithSwap-Backend/migrations/013_add_status_to_waste_logs.js
+++ EcolithSwap-Backend/migrations/013_add_status_to_waste_logs.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex) {
-  return knex.schema.table('waste_logs', function(table) {
+  return knex.schema.table('plastic_waste_logs', function(table) {
     table.enum('status', ['verified', 'pending_verification']).defaultTo('pending_verification');
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.table('waste_logs', function(table) {
+  return knex.schema.table('plastic_waste_logs', function(table) {
     table.dropColumn('status');
   });
 };
