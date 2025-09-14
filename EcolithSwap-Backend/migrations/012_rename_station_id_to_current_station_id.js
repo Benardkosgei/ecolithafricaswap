86c5b9264825995d652d019e0d355abb1a1ad9b8
benardkosgei96 <benardkosgei96@gmail.com>
2025-09-14 14:32:03 +0000

    update migration

--- EcolithSwap-Backend/migrations/012_rename_station_id_to_current_station_id.js
+++ EcolithSwap-Backend/migrations/012_rename_station_id_to_current_station_id.js
@@ -1,12 +1,11 @@
-
 exports.up = function(knex) {
   return knex.schema.table('batteries', function(table) {
-    table.renameColumn('station_id', 'current_station_id');
+    table.renameColumn('current_station_id', 'station_id');
   });
 };
 
 exports.down = function(knex) {
   return knex.schema.table('batteries', function(table) {
-    table.renameColumn('current_station_id', 'station_id');
+    table.renameColumn('station_id', 'current_station_id');
   });
 };
